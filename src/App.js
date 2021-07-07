8f5d09601b39e7eebbd786430068706a6d71d8bd
anthony romero <anthonyromeroromero014@gmail.com>
2021-07-06 20:07:21 -0400

    añadiendo homePage 1.0

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import CajaDeMensaje from './cajaDeMensaje/cajaDeMensaje'
-import LeftBar from './chat/leftBar' 
+
+import { useState } from 'react';
+import Chat from './Chat';
+import Home from './Home';
+
 
 const App=()=>{
-   return(
-       <div className="w-screen h-screen flex justify-center flex-wrap bg-gray-300 border-box">
-        <div style={{backgroundColor:"#009688"}} className="w-full h-32"></div>
-        <div style={{height:"95%",width:"75%"}} className="fixed top-5 bg-white flex">
-          <LeftBar/>
-          <CajaDeMensaje/>
-        </div> 
-        
-          
-       </div>
+  const [user,setUser]=useState(false);
+  return(
+    <>
+      {user?<Chat/>:<Home/>}
+    </>
     );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="bg-black h-screen flex justify-center flex-wrap overflow-y-scroll">
+           <div style={{height:"30%",background:"#00BFA5"}} className="w-screen flex justify-center">
+               <header style={{width:"70%"}} className="text-white">
+                    <p style={{width:"max-content"}} className="mt-8">WHATSAPP WEB</p>
+               </header>
+           </div>
+           <div className="bg-white w-3/4 h-screen relative -top-24 rounded">
+
+           </div>
+        </div>
+    )
+}
+
+export default Home
